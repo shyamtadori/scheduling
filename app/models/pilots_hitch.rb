c959d9216be33e96bd2f49b70a74525df254e0bc
Shyam <shyam.tadori@gmail.com>
2017-07-13 16:58:11 -0500

    created_by and last_updated_by for pilot_hitch

--- app/models/pilots_hitch.rb
+++ app/models/pilots_hitch.rb
@@ -8,6 +8,26 @@ class PilotsHitch < ActiveRecord::Base
 	validates_presence_of :effective_start_date, :effective_end_date
   validate :end_date_is_after_start_date
 
+  before_create do
+    self.created_by = User.current.id
+    self.last_updated_by = User.current.id
+  end
+
+  before_update do
+    self.created_by = User.current.id
+    self.last_updated_by = User.current.id
+  end
+
+  def created_user_name
+  	calendar_created_user = User.find(self.created_by)
+  	calendar_created_user.first_name
+  end
+
+  def last_upated_user_name
+  	calendar_created_user = User.find(self.last_updated_by)
+  	calendar_created_user.first_name
+  end
+
   private
   def end_date_is_after_start_date
     return if effective_end_date.blank? || effective_start_date.blank?
@@ -15,5 +35,5 @@ class PilotsHitch < ActiveRecord::Base
     if effective_end_date <= effective_start_date
       errors.add(:effective_end_date, "cannot be before the start date") 
     end 
-  end 
+  end
 end
