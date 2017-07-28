e4fd0448c21a58c7d47cdb03e2a015f28f5271e9
Shyam <shyam.tadori@gmail.com>
2017-07-28 12:07:36 -0500

    including null effective_end_date while fetching available pilots

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,7 @@ class User < ActiveRecord::Base
   def self.get_pilots_available_on(schedule_date)
     calendar_hitch_ids = CalendarHitchDate.where(work_date: schedule_date).pluck(:cal_hitch_id).map(&:to_s)
     hitch_ids = CalendarsHitch.where("cal_hitch_id in (?)",calendar_hitch_ids).pluck(:hitch_id).map(&:to_s)
-    all_available_pilot_ids = PilotsHitch.where('hitch_id in (?) and effective_start_date <= ? and effective_end_date >= ?', hitch_ids, schedule_date, schedule_date).pluck(:user_id)
+    all_available_pilot_ids = PilotsHitch.where('hitch_id in (?) and effective_start_date <= ? and (effective_end_date >= ? or effective_end_date is null)', hitch_ids, schedule_date, schedule_date).pluck(:user_id)
     pilot_ids_working_on_this_date = Schedule.where(schedule_date: schedule_date).pluck(:user_id)
     available_pilot_ids = all_available_pilot_ids - pilot_ids_working_on_this_date
     return User.where('user_id in (?)',available_pilot_ids)
